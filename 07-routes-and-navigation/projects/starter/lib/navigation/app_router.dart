558fc7961d0b634499499e8d5aa9075aac5c1578
Tawanna Hakkinen <thol_16@hotmail.com>
2022-01-25 22:24:21 -0300

    conclusão do capitulo 7

---
+++ 07-routes-and-navigation/projects/starter/lib/navigation/app_router.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+import '../screens/screens.dart';
+
+// 1
+class AppRouter extends RouterDelegate
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin {
+  // 2
+  @override
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  // 3
+  final AppStateManager appStateManager;
+  // 4
+  final GroceryManager groceryManager;
+  // 5
+  final ProfileManager profileManager;
+
+  AppRouter({
+    required this.appStateManager,
+    required this.groceryManager,
+    required this.profileManager,
+  }) : navigatorKey = GlobalKey<NavigatorState>() {
+    appStateManager.addListener(notifyListeners);
+    groceryManager.addListener(notifyListeners);
+    profileManager.addListener(notifyListeners);
+  }
+
+  @override
+  void dispose() {
+    appStateManager.removeListener(notifyListeners);
+    groceryManager.removeListener(notifyListeners);
+    profileManager.removeListener(notifyListeners);
+    super.dispose();
+  }
+
+  // 6
+  @override
+  Widget build(BuildContext context) {
+    // 7
+    return Navigator(
+      // 8
+      key: navigatorKey,
+      onPopPage: _handlePopPage,
+      // 9
+      pages: [
+        if (!appStateManager.isInitialized) SplashScreen.page(),
+        if (appStateManager.isInitialized && !appStateManager.isLoggedIn)
+          LoginScreen.page(),
+        if (appStateManager.isLoggedIn && !appStateManager.isOnboardingComplete)
+          OnboardingScreen.page(),
+        if (appStateManager.isOnboardingComplete)
+          Home.page(appStateManager.getSelectedTab),
+        if (groceryManager.isCreatingNewItem)
+          GroceryItemScreen.page(
+              onCreate: (item) {
+                groceryManager.addItem(item);
+              },
+              onUpdate: (item, index) {}),
+        if (groceryManager.selectedIndex != -1)
+          GroceryItemScreen.page(
+              item: groceryManager.selectedGroceryItem,
+              index: groceryManager.selectedIndex,
+              onCreate: (_) {},
+              onUpdate: (item, index) {
+                groceryManager.updateItem(item, index);
+              }),
+        if (profileManager.didSelectUser)
+          ProfileScreen.page(profileManager.getUser),
+        if (profileManager.didTapOnRaywenderlich) WebViewScreen.page(),
+      ],
+    );
+  }
+
+  bool _handlePopPage(Route<dynamic> route, result) {
+    if (!route.didPop(result)) {
+      return false;
+    }
+
+    // 5
+    if (route.settings.name == FooderlichPages.onboardingPath) {
+      appStateManager.logout();
+    }
+    if (route.settings.name == FooderlichPages.groceryItemDetails) {
+      groceryManager.groceryItemTapped(-1);
+    }
+    if (route.settings.name == FooderlichPages.profilePath) {
+      profileManager.tapOnProfile(false);
+    }
+    if (route.settings.name == FooderlichPages.raywenderlich) {
+      profileManager.tapOnRaywenderlich(false);
+    }
+
+    return true;
+  }
+
+  // 10
+  @override
+  Future<void> setNewRoutePath(configuration) async => null;
+}
